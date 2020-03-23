c3969028cb6425461cf5fd80777952cab08d3a01
iL53n <ilsegribko@gmail.com>
2020-03-23 12:56:49 +0300

    t01 add Task1 class

--- HW02/Ilya_Gribko/ilya_gribko_hw_02_t_01.rb
+++ HW02/Ilya_Gribko/ilya_gribko_hw_02_t_01.rb
@@ -2,17 +2,35 @@
 # * Full text of the first line with an error (the presence of the substring error in any case)
 # * If the input is text that does not contain the word error, return an empty string
 
-def task_1(file_name)
-  File.foreach(file_name) do |f|
-    if error?(f)
-      puts f
-      break # only first error line
+class Task1
+  def initialize
+    @error = false
+    task_1('data_test.log')
+  end
+
+  def task_1(file_name)
+    File.foreach(file_name) do |f|
+
+
+      if error?(f)
+        error_line(f)
+        break # only first error line
+      end
     end
+    puts '' unless @error # return an empty line
+  end
+
+  private
+
+  def error?(line)
+    line.include?('error')
+  end
+
+  def error_line(line)
+    puts line
+    @error = true
   end
 end
 
-def error?(line)
-  line.include?('error')
-end
+Task1.new
 
-task_1('data_test.log')
