3a63d3143133999964ba2481265cd7786d29041f
iL53n <ilsegribko@gmail.com>
2020-03-24 15:49:04 +0300

    t04 add regexp

---
+++ HW02/Ilya_Gribko/ilya_gribko_hw_02_t_04.rb
@@ -0,0 +1,11 @@
+# 4. An arbitrary string is given, the method task_4 must return the count of letters and digits in that string.
+#  Example:
+#  task_4("hel2!lo") # --> {letters: 5, digits: 1}
+#  task_4("wicked .. !") # --> 6 --- {letters: 6, digits: 0}
+
+LETTERS = /[a-zA-Z]/.freeze
+DIGITS = /[0-9]/.freeze
+
+def task_4(string)
+
+end
