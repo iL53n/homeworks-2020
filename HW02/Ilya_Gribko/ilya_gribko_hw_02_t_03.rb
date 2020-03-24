8b6ed19ec8cd36c1ea89f62eff10de2074310cfd
iL53n <ilsegribko@gmail.com>
2020-03-24 15:39:16 +0300

    t03 done for check_rubocop passed

--- HW02/Ilya_Gribko/ilya_gribko_hw_02_t_03.rb
+++ HW02/Ilya_Gribko/ilya_gribko_hw_02_t_03.rb
@@ -5,9 +5,9 @@
 # * For this example, the output will be:
 # (2018-04-23 17:18:38.8 - 2018-04-23 17:17:49.7) = (38.8 + 10.3) = "49.1"
 # <- only this number is a string
-# [ ] * If there are no valid events at the entrance or it is the only one
+# [X] * If there are no valid events at the entrance or it is the only one
 #       to return "0"
-# [ ] * If there are many events, the methods should return an array of duration
+# [X] * If there are many events, the methods should return an array of duration
 # between 1st and 2nd, 2nd and 3rd and etc. Example: ["40.1", "42.2"]
 require 'date'
 
@@ -16,17 +16,21 @@ class Task3
 
   def initialize(file_name)
     @arr_data = []
-    # @arr_durations = []
-    task_3(file_name)
+    @arr_durations = []
+    task_3(file_name) if check_file_name?(file_name)
   end
 
   def task_3(file_name)
     parse_log_file(file_name)
     output_result
-    # output_array_durations
+    output_array_durations
   end
 
   private
+
+  def check_file_name?(file_name)
+    File.exist?(file_name) ? true : puts('File not found!')
+  end
 
   def parse_log_file(file_name)
     File.foreach(file_name) do |f|
@@ -39,15 +43,24 @@ class Task3
     while n < @arr_data.count - 1
       reduced = @arr_data[n + 1]
       subtracted = @arr_data[n]
-      difference = (reduced[:seconds] - subtracted[:seconds]).round(1).to_s
+      @arr_durations << difference = difference(reduced, subtracted)
       puts "(#{reduced[:timedate]} - #{subtracted[:timedate]}) = #{difference}"
       n += 1
     end
   end
 
-  # def output_array_durations
-  #   puts @arr_durations
-  # end
+  def difference(reduced, subtracted)
+    (reduced[:seconds] - subtracted[:seconds]).round(1).to_s
+  end
+
+  def output_array_durations
+    if @arr_durations.any?
+      puts 'Array of duration between events:'
+      puts @arr_durations
+    else
+      puts 0
+    end
+  end
 
   def format_match?(line)
     line.include?('Calling core with action:') &&
