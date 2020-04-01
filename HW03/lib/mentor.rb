b3133560d940f7eae61d6eee11d64d1444eb79af
iL53n <ilsegribko@gmail.com>
2020-04-01 15:55:56 +0300

    add Mentor class

---
+++ HW03/lib/mentor.rb
@@ -0,0 +1,15 @@
+class Mentor
+  def initialize(name:, surname:)
+    @name = name
+    @surname = surname
+    @notifications = []
+  end
+
+  def subscribe_to(student)
+
+  end
+
+  def read_notifications!
+
+  end
+end
