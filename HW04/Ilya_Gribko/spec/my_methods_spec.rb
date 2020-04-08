1e9c485ace1afeb8e7b84ea8cdf5ca96f833d814
iL53n <ilsegribko@gmail.com>
2020-04-08 22:44:24 +0300

    add spec file to my methods

---
+++ HW04/Ilya_Gribko/spec/my_methods_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../lib/my_methods'
+
+RSpec.describe Array do
+  subject { described_class.new((1..10).to_a) }
+
+  describe '#my_each' do
+
+  end
+
+  describe '#my_map' do
+
+  end
+
+  describe '#my_select' do
+
+  end
+end
