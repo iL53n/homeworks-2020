0b3b331d04f42e6a6da9981eb17f757a2d33702a
iL53n <ilsegribko@gmail.com>
2020-04-08 20:56:40 +0300

    hw04 init

---
+++ HW04/Ilya_Gribko/app.rb
@@ -0,0 +1,7 @@
+# [ ] Implement your own Array#my_map, Array#my_select, Array#my_each.
+# [ ] Write specs for each of your methods using RSpec.
+# Notes: implement these methods in your custom module and then include it in the Array class.
+# Important: Do not use #each, #select, #map in your implementation.
+
+require_relative 'lib/my_methods'
+
---
+++ HW04/Ilya_Gribko/lib/my_methods.rb
@@ -0,0 +1,13 @@
+# my_map, my_select, my_each.
+
+def my_map
+
+end
+
+def my_select
+
+end
+
+def my_each
+
+end
