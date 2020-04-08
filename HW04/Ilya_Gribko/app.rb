5f0d80cf37e05e6b3a7d0673335d71bcdeb79baf
iL53n <ilsegribko@gmail.com>
2020-04-08 21:43:11 +0300

    add my_select method

--- HW04/Ilya_Gribko/app.rb
+++ HW04/Ilya_Gribko/app.rb
@@ -17,10 +17,18 @@ test_array = [0, 1, 1, 2, 3, 5, 8]
 
 ### my_map
 # { }
-new_array = test_array.my_map { |i| puts i += 1 }
-puts new_array.class == Array
+# new_array = test_array.my_map { |i| puts i += 1 }
+# puts new_array.class == Array
 # do-end
-new_array = test_array.my_map do |i|
+# new_array = test_array.my_map do |i|
+#   puts i
+# end
+# puts new_array.class == Array
+
+### my_select
+# { }
+test_array.my_select { |i| puts i += 1 }
+# do-end
+test_array.my_select do |i|
   puts i
 end
-puts new_array.class == Array
--- HW04/Ilya_Gribko/lib/my_methods.rb
+++ HW04/Ilya_Gribko/lib/my_methods.rb
@@ -3,7 +3,10 @@
 # array.each { |item| block }
 # 2. Array#my_map
 # Executes code in block for each element of the array and return *new* array
-# array.map {|item| block }  #=> new_array
+# array.map {|item| block }  #=> array
+# 3. Array#select
+# Returns an array containing all `enum` elements for which the result of processing block is not `false`
+# enum.select {| obj | block }  => array
 
 class Array
   def my_each
@@ -27,7 +30,15 @@ class Array
   end
 
   def my_select
+    return_array = []
 
+    i = 0
+    while i < size
+      return_array << yield(self[i])
+      i += 1
+    end
+
+    return_array
   end
 end
 
