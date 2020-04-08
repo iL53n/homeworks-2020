7e23ec299ba107fad4501e160561907e858b9611
iL53n <ilsegribko@gmail.com>
2020-04-08 21:32:53 +0300

    add my_map method

--- HW04/Ilya_Gribko/lib/my_methods.rb
+++ HW04/Ilya_Gribko/lib/my_methods.rb
@@ -1,6 +1,9 @@
 # 1. Array#my_each
 # Executes code in block for each element of the array
-# array.each { |item| block }  #=> array
+# array.each { |item| block }
+# 2. Array#my_map
+# Executes code in block for each element of the array and return *new* array
+# array.map {|item| block }  #=> new_array
 
 class Array
   def my_each
@@ -12,7 +15,15 @@ class Array
   end
 
   def my_map
+    return_array = []
 
+    i = 0
+    while i < size
+      return_array << yield(self[i])
+      i += 1
+    end
+
+    return_array
   end
 
   def my_select
