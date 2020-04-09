08d7bda4e0b96223368396a2f5776c1a998d700b
iL53n <ilsegribko@gmail.com>
2020-04-09 11:48:49 +0300

    test_return Enumenator object if block not given

--- HW04/Ilya_Gribko/spec/my_methods_spec.rb
+++ HW04/Ilya_Gribko/spec/my_methods_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/my_methods'
 
-RSpec.describe Array do
+RSpec.describe Array, type: :class do
   subject { described_class.new((1..10).to_a) }
 
   describe '#my_each' do
@@ -10,9 +10,13 @@ RSpec.describe Array do
       end
     end
 
-    context 'block not given' do
-      it 'return Enumerator and array:my_each' do
-
+    context 'block not given' do # TODO: add shared example
+      it 'return Enumerator object with original array' do
+        # option 1
+        expect(subject.my_each).to be_an_instance_of(Enumerator)
+        expect(subject.my_each.to_a).to match_array(subject)
+        # option 2
+        expect(subject.my_each.inspect).to eq(subject.to_enum(:my_each).inspect)
       end
     end
   end
@@ -25,8 +29,9 @@ RSpec.describe Array do
     end
 
     context 'block not given' do
-      it 'return Enumerator and array:my_map' do
-
+      it 'return Enumerator object with original array' do
+        expect(subject.my_map).to be_an_instance_of(Enumerator)
+        expect(subject.my_each.to_a).to match_array(subject)
       end
     end
   end
@@ -39,8 +44,9 @@ RSpec.describe Array do
     end
 
     context 'block not given' do
-      it 'return Enumerator and array:my_select' do
-
+      it 'return Enumerator object with original array' do
+        expect(subject.my_select).to be_an_instance_of(Enumerator)
+        expect(subject.my_each.to_a).to match_array(subject)
       end
     end
   end
